cd4c6e87399b50153c9cc7365e6f1b77b3176b02
Alvaro Chavez <a.chavezs@duocuc.cl>
2026-06-10 01:10:49 -0400

    feat: conexión notificaciones

---
+++ src/main/java/cl/pymetrack/msnotificaciones/config/RabbitJsonConfig.java
@@ -0,0 +1,14 @@
+package cl.pymetrack.msnotificaciones.config;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitJsonConfig {
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
--- src/main/java/cl/pymetrack/msnotificaciones/consumer/PedidoEstadoConsumer.java
+++ src/main/java/cl/pymetrack/msnotificaciones/consumer/PedidoEstadoConsumer.java
@@ -1,23 +1,24 @@
 package cl.pymetrack.msnotificaciones.consumer;
 
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class PedidoEstadoConsumer {
 
     @RabbitListener(queues = "pedido.estado.actualizado")
-    public void consumirCambioEstado(Message message) {
-        String exchange = message.getMessageProperties().getReceivedExchange();
-        String routingKey = message.getMessageProperties().getReceivedRoutingKey();
-        String contentType = message.getMessageProperties().getContentType();
-
+    public void consumirCambioEstado(Map<String, Object> event) {
         System.out.println("======================================");
         System.out.println("📩 EVENTO RECIBIDO DESDE RABBITMQ");
-        System.out.println("Exchange: " + exchange);
-        System.out.println("Routing key: " + routingKey);
-        System.out.println("Content type: " + contentType);
+        System.out.println("Pedido ID: " + event.get("pedidoId"));
+        System.out.println("PYME ID: " + event.get("idPyme"));
+        System.out.println("Estado anterior: " + event.get("estadoAnterior"));
+        System.out.println("Estado nuevo: " + event.get("estadoNuevo"));
+        System.out.println("Repartidor ID: " + event.get("repartidorId"));
+        System.out.println("Observación: " + event.get("observacion"));
+        System.out.println("Fecha evento: " + event.get("fechaEvento"));
         System.out.println("📧 Simulando envío de correo al cliente...");
         System.out.println("✅ Correo enviado correctamente");
         System.out.println("======================================");
